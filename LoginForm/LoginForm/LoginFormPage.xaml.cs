984a6ede417cce2854efd2feeff91db7b0e6eeb5
Kalyani Nikumbh <sachin.nikumbh7@gmail.com>
2017-05-24 22:55:30 +0530

    Initial Commit

---
+++ LoginForm/LoginForm/LoginFormPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using LoginForm.ViewModel;
+
+namespace LoginForm
+{
+	public partial class LoginFormPage : ContentPage
+	{
+		public LoginFormPage()
+		{
+			var objLoginFormPageViewModel = new LoginFormPageViewModel();
+			this.BindingContext = objLoginFormPageViewModel;
+			objLoginFormPageViewModel.DisplayInvalidLoginPrompt += () => DisplayAlert("Alert", "Shree Swami Samarth", "Okay");
+			InitializeComponent();
+		}
+	}
+}
---
+++ LoginForm/LoginForm/ViewModel/LoginFormPageViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace LoginForm.ViewModel
+{
+	public class LoginFormPageViewModel : INotifyPropertyChanged
+	{
+		#region INotifyPropertyChange Implementation
+		public event PropertyChangedEventHandler PropertyChanged = delegate
+		{
+
+		};
+		#endregion
+
+		public Action DisplayInvalidLoginPrompt;
+
+		private string emailAddress;
+
+		public string EmailAddress
+		{
+			get
+			{
+				return emailAddress;
+			}
+			set
+			{
+				emailAddress = value;
+				PropertyChanged(this, new PropertyChangedEventArgs("EmailAddress"));
+			}
+		}
+
+
+		private string password;
+
+		public string Password
+		{
+			get
+			{
+				return password;
+			}
+			set
+			{
+				password = value;
+				PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+			}
+		}
+
+		public ICommand SubmitCommand { get; protected set; }
+
+		public LoginFormPageViewModel()
+		{
+			SubmitCommand = new Command(OnSubmit);
+		}
+
+		public void OnSubmit()
+		{
+			if (emailAddress != "sss" || password != "s")
+			{
+				DisplayInvalidLoginPrompt();
+			}
+		}
+
+
+	}
+}
